2375331b5ff895154dffe6770196221e1168f119
chengxiang.peng <chengxiang.peng@qunar.com>
2016-06-09 23:47:08 +0800

    添加hotel,flight,common模块

---
+++ flight/src/main/java/com/qunar/flight/FlightActivity.java
@@ -0,0 +1,13 @@
+package com.qunar.flight;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FlightActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flight);
+    }
+}
