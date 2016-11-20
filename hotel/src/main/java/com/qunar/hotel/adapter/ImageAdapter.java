c1c38018f420e80dc123c2ea2aa05fd77f521887
chengxiang.peng <chengxiang.peng@qunar.com>
2016-11-21 00:00:18 +0800

    移除Log import

--- hotel/src/main/java/com/qunar/hotel/adapter/ImageAdapter.java
+++ hotel/src/main/java/com/qunar/hotel/adapter/ImageAdapter.java
@@ -2,7 +2,6 @@ package com.qunar.hotel.adapter;
 
 import android.content.Context;
 import android.content.res.Resources;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
