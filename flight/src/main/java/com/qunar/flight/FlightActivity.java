a4f16d97ad88eb1921ba94226dd8dab398c6f23f
chengxiang.peng <chengxiang.peng@qunar.com>
2016-12-23 20:25:29 +0800

    矢量图的实践

--- flight/src/main/java/com/qunar/flight/FlightActivity.java
+++ flight/src/main/java/com/qunar/flight/FlightActivity.java
@@ -1,7 +1,12 @@
 package com.qunar.flight;
 
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ReplacementTransformationMethod;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class FlightActivity extends AppCompatActivity {
 
@@ -9,5 +14,11 @@ public class FlightActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flight);
+
+//        ImageView imageView1 = (ImageView) findViewById(R.id.imageview1);
+//        Drawable drawable1 = imageView1.getDrawable();
+//        if (drawable1 instanceof Animatable){
+//            ((Animatable) drawable1).start();
+//        }
     }
 }
