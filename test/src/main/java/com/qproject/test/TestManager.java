1d7cac945c80873c62d9016a262b3b8b02c90e2f
chengxiang.peng <chengxiang.peng@qunar.com>
2016-12-25 22:21:52 +0800

    1.1添加单例内存内存泄露注释

--- test/src/main/java/com/qproject/test/TestManager.java
+++ test/src/main/java/com/qproject/test/TestManager.java
@@ -11,6 +11,12 @@ public class TestManager {
     private static TestManager instance;
     private Context context;
 
+    /**
+     * 传入的Context的生命周期很重要：
+     *   如果传入的是Application的Context，则生命周期和单例生命周期一样长；
+     *   如果传入的是Activity的Context，由于该Context和Activity的生命周期一样长，当Activity退出的时候它的内存不会被回收，因为单例对象持有它的引用；
+     * @param context
+     */
     private TestManager(Context context) {
         this.context = context;
     }
